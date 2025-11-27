3159320a5ed43afdff63e1dc6877fa26460352e1
Youssef Emad <joe@Youssefs-MacBook-Pro.local>
2025-11-27 13:50:47 +0200

    Try alternative handler export format

--- api/index.py
+++ api/index.py
@@ -5,14 +5,16 @@ import os
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Add parent directory to Python path
-sys.path.insert(0, parent_dir)
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 
 # Change working directory to project root for template resolution
 os.chdir(parent_dir)
 
 # Import Flask app
-from app import app
+from app import app as flask_app
 
-# Export handler - Vercel expects this name
-handler = app
+# Vercel Python runtime handler
+# Export the Flask WSGI application directly
+handler = flask_app
 
